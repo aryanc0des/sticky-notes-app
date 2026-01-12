3d43279d4abcc5f4c6aa1e4a68e9eb13177f64ed
aryanc0des <134934556+aryanc0des@users.noreply.github.com>
2026-01-12 16:42:27 +0530

    update note by id PUT route

--- backend/main.py
+++ backend/main.py
@@ -11,9 +11,11 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=db_engine)
 
+
 class addNewNote(BaseModel):
     title: str = Field(min_length=1, max_length=30)
     description: str = Field(min_length=1)
+
 
 def get_db():
     db = LocalSession()
@@ -21,27 +23,31 @@ def get_db():
         yield db
     finally:
         db.close()
-        
+
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
 @app.get('/', status_code=status.HTTP_200_OK)
 async def home():
-    return {"message":"ok, server working"}
+    return {"message": "ok, server working"}
+
 
 @app.get('/notes', status_code=status.HTTP_200_OK)
 async def get_all_notes(db: db_dependency):
-    notes_to_return = db.query(Notes).all() 
+    notes_to_return = db.query(Notes).all()
     return notes_to_return
+
 
 @app.post('/notes/new_note', status_code=status.HTTP_201_CREATED)
 async def create_new_note(db: db_dependency, new_note: addNewNote):
     note_to_add = Notes(**new_note.model_dump())
-    
+
     db.add(note_to_add)
     db.commit()
-    
+
     raise HTTPException(status_code=201, detail='new note created sucessfully')
+
 
 @app.get('/notes/detailed_note/{id}', status_code=status.HTTP_200_OK)
 async def get_note_by_id(db: db_dependency, id: int):
@@ -50,3 +56,18 @@ async def get_note_by_id(db: db_dependency, id: int):
         return note_to_return
     else:
         raise HTTPException(status_code=404, detail='note not found')
+
+
+@app.put('/notes/update_note/{id}', status_code=status.HTTP_204_NO_CONTENT)
+async def update_note(db: db_dependency, id: int, update_this_note: addNewNote):
+    note_to_update = db.query(Notes).filter_by(id=id).first()
+
+    if note_to_update is None:
+        raise HTTPException(status_code=404, detail='note not found')
+    
+    note_to_update.title = update_this_note.title
+    note_to_update.description = update_this_note.description
+    
+    db.add(note_to_update)
+    db.commit()
+    raise HTTPException(status_code=204, detail='updated the existing note sucessfully')
